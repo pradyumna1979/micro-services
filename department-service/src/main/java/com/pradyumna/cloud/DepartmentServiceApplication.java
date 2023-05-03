f3fe14af39cf3be1e674f07395bc130d77436646
pradyumna1979 <kuni.khadanga@gmail.com>
2023-05-03 23:50:19 +0530

    added changes for HttpExchange client for microservices intercommunications

--- department-service/src/main/java/com/pradyumna/cloud/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/pradyumna/cloud/DepartmentServiceApplication.java
@@ -4,10 +4,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-
 @SpringBootApplication
-@EnableFeignClients
 @OpenAPIDefinition(info =
 @Info(title = "Organization API", version = "1.0", description = "Documentation Organization API v1.0")
 )
